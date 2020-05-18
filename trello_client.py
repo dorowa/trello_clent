55bd23b64aee018860195d00bd70728c3b7d25f9
dorowa <dorowa@gmail.com>
2020-05-18 19:54:11 +0300

    Update trello_client.py

--- trello_client.py
+++ trello_client.py
@@ -1,59 +1,170 @@
 import requests
-import sys
 import json
 
 base_url = "https://api.trello.com/1/{}"
 config_path = "../trello.cfg"
 board_id = "pF5Bin10"
-
+long_board_id = '5ebc2f2f7571cb41c74553c6'
 auth_params = None
 
+width_ = 77
+jobs_list = []
+
 def getJSONdata(filename):
-	"""получаем данные из файла"""
+	"""получаем конфигурационные данные из файла
+    в файле данные хранятся в виде"""
 	fileJSON = open(filename)
 	dataRaws = fileJSON.readlines()
 	return json.loads(dataRaws[0])
 
 def read():
+    """Получаем список всех столбцов и заданий в столбцах,
+    чтобы изначально избежать проблем с дубликатами названий как заданий
+    так и столбцов, формируем список словарей, в которые сохраняем уникальные
+    id и столбцов, и уникальные id всех заданий, при этом создаем сквозную
+    нумерацию от 1++ для отображения всех элементов на экране, в таком варианте
+    у нас будет однозначная связь: 'номер на экране' - id"""
+    global jobs_list
+    if jobs_list:
+        jobs_list = []
     column_data = requests.get(f"{base_url.format('boards')}/{board_id}/lists",params=auth_params).json()
+    index_ = 1
+    column_idx = 1
     for column in column_data:
-        print(column['name'])
+        tasks_ = []
         task_data = requests.get(f"{base_url.format('lists')}/{column['id']}/cards",params=auth_params).json()
         if not task_data:
-            print(f'\t {"Нет задач"}')
-            continue
-        for task in task_data:
-            print(f"\t {task['name']}")
-def create(name, column_name):
-    column_data = requests.get(f"{base_url.format('boards')}/{board_id}/lists",params=auth_params).json()
-    for column in column_data:
-        if column['name']==column_name:
-            requests.post(base_url.format('cards'), data={'name':name, 'idList':column['id'], **auth_params})
-            break
+            tasks_ = []
+        else:
+            for task in task_data:
+                tasks_.append({"list_id":index_, "id":task['id'],"name":task['name']})
+                index_ += 1
+        jobs_list.append({"list_column_idx":column_idx, "id":column['id'], 'name':column['name'],"tasks":tasks_})
+        column_idx +=1
 
-def move(name, column_name):
-    column_data = requests.get(f"{base_url.format('boards')}/{board_id}/lists",params=auth_params).json()
-    task_id = None
-    for column in column_data:
-        column_tasks = requests.get(f"{base_url.format('lists')}/{column['id']}/cards",params=auth_params).json()
-        for task in column_tasks:
-            if task['name'] == name:
-                task_id = task['id']
-                break
-        if task_id:
-            break
-    for column in column_data:
-        if column['name'] == column_name:
-            requests.put(f"{base_url.format('cards')}/{task_id}/idList",data={'value': column['id'], **auth_params})
-            break
+def create_column(column_name):
+    requests.post(base_url.format('lists'), data = {'name':column_name, 'idBoard': long_board_id, **auth_params})
+
+def create(name, column_id):
+    requests.post(base_url.format('cards'), data={'name':name, 'idList':column_id, **auth_params})
+
+def move(task_id, column_id):
+    requests.put(f"{base_url.format('cards')}/{task_id}/idList",data={'value': column_id, **auth_params})
+
+def get_column_id_by_list_id(list_id):
+    """получаем реальный id столбца по номеру в списке, если столбец не найден возвращаем False"""
+    for jobs in jobs_list:
+        if jobs['list_column_idx']==list_id:
+            return jobs['id']
+    return False
+
+def get_task_id_by_list_id(list_id):
+    """получаем реальный id задания по номеру в списке, если не найден возвращаем False"""
+    for jobs in jobs_list:
+        for job in jobs['tasks']:
+            if job['list_id']==list_id:
+                return job['id']
+    return False
+
+def main_menu(printJobsList):
+    # --- выводим заголовок
+    print('\n'*15) # сдвинем вывод в окне, чтобы не прилипать к предыдущему выводу
+    print(f'+{"="*25}{"+"}{"="*25}{"+"}{"="*25}+')
+    print(f'| L - Показать все задачи |   A - Добавить задачу   |   M - Изменить задачу   |')
+    print(f'|{"-"*25}{"+"}{"-"*25}{"+"}{"-"*25}|')
+    print(f'| C - Добавить столбец    |   R - Обновить список   |   Q - Выход             |')
+    print(f'+{"="*25}{"+"}{"="*25}{"+"}{"="*25}+')
+    # --- конец заголовка, дальше выводим список столбцов и задач в столбцах, полученных read()
+    lines_count = 0
+    if not printJobsList:
+        print('\n'*16) # если список пуст просто выводим меню, это такая очистка экрана
+    else:
+        #вывод списка столбцов и задач в столбцах, добавим немножк форматирования, для красоты
+        for jobs in printJobsList:
+            str_column = f"| {jobs['list_column_idx']} - {jobs['name']}, задач: {len(jobs['tasks'])}"
+            print(f"{str_column}{' '*(width_ - len(str_column)+1)}|")
+            if len(jobs['tasks'])==0:
+                print(f"|{' '*77}|")
+            for task in jobs['tasks']:
+                print(f"|     {task['list_id']} - {task['name']}{' '*(width_ - len(str(task['list_id'])) - len(task['name']) - 8)}|")
+                lines_count +=1
+            print(f"|{' '*77}|")
+            lines_count += 2
+        if lines_count <15:
+            print("\n"*(15-lines_count-1))
+    # под телом вывода, выведем футер с подсказкой команды для выхода
+    print(f'\n+{"="*77}+')
+    print(f"| Для завершения работы введите: q{' '*44}|")
+    print(f'+{"="*77}+')
 
 if __name__ == "__main__":
     auth_params = getJSONdata(config_path)
-    if len(sys.argv)<=3:
-        read()
-    elif sys.argv[1]=='create':
-        create(sys.argv[2],sys.argv[3])
-    elif sys.argv[1]=='move':
-        move(sys.argv[2], sys.argv[3])
-    else:
-        print('Неверный аргумент')
+    command_ = None
+    main_menu([])
+    read()
+    while True:
+        command_ = input("Введите команду:")
+        if command_ == "L":
+            main_menu(jobs_list)
+        elif command_ =="A":
+            # Добавляем новое задание
+            print('Добавим новое задание!')
+            column_id_ = input('Выберите столбец (номер слева от названия столбца): ')
+            task_name_ = input('Введите задание (текст задания): ')
+            try:
+                column_id_ = int(column_id_)
+            except:
+                print('Ошибка! Такого столбца нет!')
+                input()
+            else:
+                print('Добавляю...')
+                column_id_ = get_column_id_by_list_id(column_id_)
+                if column_id_:
+                    create(task_name_, column_id_)
+                    print('Обновляю...')
+                    read()
+                else:
+                    print('Ошибка! такого столбца нет!')
+                    input()                
+                main_menu(jobs_list)
+        elif command_ == "M":
+            # Переместить задиние (какбэ изменяем)
+            print('Перемещаем задание!')
+            task_id_ = input('Выберите задание (номер слева от названия задания): ')
+            column_id_ = input('Выберите целевой столбец (номер слева от названия столбца), куда переместить столбец: ')
+            try:
+                column_id_ = int(column_id_)
+            except:
+                print('Ошибка! Неверный столбец!')
+            else:
+                try:
+                    task_id_ = int(task_id_)
+                except:
+                    print('Ошибка! Неверное задание')
+                else:
+                    print('Перемещаем...')
+                    column_id_ = column_id_ = get_column_id_by_list_id(column_id_)
+                    if not column_id_:
+                        print('Отмена! Такого столбца нет!')
+                        input()
+                    else:
+                        task_id_ = get_task_id_by_list_id(task_id_)
+                        if task_id_:
+                            move(task_id_, column_id_)
+                            read()
+                        else:
+                            print('Отмена! Такой задачи нет!')
+                            input()
+                    main_menu(jobs_list)
+        elif command_ == "C":
+            column_name_ = input("Введите название новой колонки: ")
+            create_column(column_name_)
+            read()
+            main_menu(jobs_list)
+        elif command_ == "R":
+            read()
+            main_menu(jobs_list)
+        elif command_ in ["q", "Q"]:
+            break
+        else:
+            main_menu([])
